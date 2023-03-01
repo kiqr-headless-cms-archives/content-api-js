fe1b889b3fa5cd5118a4c2c23c09155d77116ede
Rasmus <2277443+kjellberg@users.noreply.github.com>
2023-03-01 19:28:47 +0100

    reset

--- src/models/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Document';
-export * from './DocumentsCollection';
-export * from './DocumentsCollectionMeta';
-export * from './PaginationMeta';
